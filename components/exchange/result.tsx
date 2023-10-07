e3dcf5be7192c996ae5862cd3f6bf79b32dc79ec
Mohamed Gado <boogado@yahoo.com>
2023-10-07 13:58:51 +0300

    feat(result.tsx): add 1 second delay after the user input (debounce)

--- components/exchange/result.tsx
+++ components/exchange/result.tsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import { Loader } from 'lucide-react'
 import { toast } from 'sonner'
+import { useDebouncedCallback } from 'use-debounce'
 
 import { numberFormat } from '@/lib/utils'
 import { useSearchQueryParams } from '@/hooks/use-search-params'
@@ -14,26 +15,36 @@ export const Result = () => {
   const [result, setResult] = React.useState<number>()
   const [isLoading, setIsLoading] = React.useState<boolean>()
 
-  React.useEffect(() => {
-    const fetchExchangeResult = async () => {
-      if (!toExchangeValue || !fromExchangeValue || !amountExchangeValue) return
-      setIsLoading(true)
-      const response = await getExchangePrice({
-        from: fromExchangeValue,
-        to: toExchangeValue,
-        quantity: amountExchangeValue,
-      })
+  const fetchExchangeResult = async () => {
+    if (!toExchangeValue || !fromExchangeValue || !amountExchangeValue) return
+    setIsLoading(true)
+    const response = await getExchangePrice({
+      from: fromExchangeValue,
+      to: toExchangeValue,
+      quantity: amountExchangeValue,
+    })
 
-      if (response || response === 0) {
-        setResult(response)
-      } else {
-        toast.error('Something went wrong, please try again later.')
-      }
-      setIsLoading(false)
+    if (response || response === 0) {
+      setResult(response)
+    } else {
+      toast.error('Something went wrong, please try again later.')
     }
+    setIsLoading(false)
+  }
 
-    fetchExchangeResult()
-  }, [amountExchangeValue, fromExchangeValue, toExchangeValue])
+  const debouncedCallBack = useDebouncedCallback(async () => {
+    await fetchExchangeResult()
+    // 1 second delay
+  }, 1000)
+
+  React.useEffect(() => {
+    debouncedCallBack()
+  }, [
+    amountExchangeValue,
+    debouncedCallBack,
+    fromExchangeValue,
+    toExchangeValue,
+  ])
 
   return (
     <div className="flex w-full px-8 md:p-0">
